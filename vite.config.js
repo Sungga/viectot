5d82a863344c1d62e3a072d8977038ca31636ecf
Sungga <anhvu17003@gmail.com>
2025-06-25 10:52:49 +0700

    change vite for deploy

--- vite.config.js
+++ vite.config.js
@@ -1,41 +1,41 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
-
-export default defineConfig({
-    // server: {
-    //     host: '0.0.0.0', // Cho phép truy cập từ bên ngoài
-    //     port: 5173, // Cổng Vite
-    //     hmr: {
-    //         protocol: 'ws', // Đảm bảo WebSocket hoạt động
-    //     }
-    // },
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
-});
-
-
-
-
-// import { defineConfig } from 'vite'
-// import laravel from 'laravel-vite-plugin'
-// import fg from 'fast-glob'
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+// import tailwindcss from '@tailwindcss/vite';
 
 // export default defineConfig({
+//     // server: {
+//     //     host: '0.0.0.0', // Cho phép truy cập từ bên ngoài
+//     //     port: 5173, // Cổng Vite
+//     //     hmr: {
+//     //         protocol: 'ws', // Đảm bảo WebSocket hoạt động
+//     //     }
+//     // },
 //     plugins: [
 //         laravel({
-//             input: [
-//                 // Quét tất cả file CSS trong resources/css và subfolders
-//                 ...fg.sync('resources/css/**/*.css'),
-//                 // Quét tất cả file JS trong resources/js và subfolders
-//                 ...fg.sync('resources/js/**/*.js'),
-//             ],
+//             input: ['resources/css/app.css', 'resources/js/app.js'],
 //             refresh: true,
 //         }),
+//         tailwindcss(),
 //     ],
-// })
+// });
+
+
+
+
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import fg from 'fast-glob'
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                // Quét tất cả file CSS trong resources/css và subfolders
+                ...fg.sync('resources/css/**/*.css'),
+                // Quét tất cả file JS trong resources/js và subfolders
+                ...fg.sync('resources/js/**/*.js'),
+            ],
+            refresh: true,
+        }),
+    ],
+})
